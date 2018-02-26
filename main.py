740af0687218153ab4939c1623e4559a2f3a0b1c
richarddevilla <chaseyba@gmail.com>
2018-02-27 01:26:51 +1100

    Made code reusable

--- main.py
+++ main.py
@@ -1,25 +1,46 @@
 from datetime import datetime
 
-
-def calculateAge():
+def inputDOB():
     while True:
-        try:
-            dob = input('Input your date of birth(dd/mm/yyyy): ')
-            currentDate = datetime.now()
-            dobParsed = datetime.strptime(dob, '%d/%m/%Y')
-            age = currentDate.year - dobParsed.year
-            if (currentDate.month, currentDate.day) < (dobParsed.month, dobParsed.day):
-                age -= 1
-            assert age > 0
-        except ValueError:
-            print('Please input a valid date of birth!')
-        except AssertionError:
-            print('Unless you are a time traveller, I doubt you were born on ' + dob + '.')
-        except:
-            print('Unexpected error! Please try again.')
+        dob = input('Input your date of birth(dd/mm/yyyy): ')
+        if validateDOB(dob):break
+    return validateDOB(dob)
+
+def validateDOB(dob):
+    try:
+        dobParsed = datetime.strptime(dob, '%d/%m/%Y')
+        assert dobParsed < datetime.now()
+        return dobParsed
+    except ValueError:
+        print('Please input a valid date of birth!')
+    except AssertionError:
+        print('Unless you are a time traveller, I doubt you were born on ' + str(dobParsed) + '.')
+    except:
+        print('Unexpected error! Please try again.')
+    return False
+
+def calculateAge(dobParsed):
+    currentDate = datetime.now()
+    age = currentDate.year - dobParsed.year
+    if (currentDate.month, currentDate.day) < (dobParsed.month, dobParsed.day):
+        age -= 1
     return age
 
+def inputName():
+    while True:
+        name = input('Input your name: ')
+        if validateName(name):break
+    return name
 
-name = input('Input your name: ')
+def validateName(name):
+    if name.isalpha():
+        return True
+    else:
+        print('Please input letters only!')
+        return False
 
-print(calculateAge())
+name = inputName()
+dobParsed = inputDOB()
+age = calculateAge(dobParsed)
+print('Hi! ' + name)
+print('You are ' + str(age) + ' years old')
